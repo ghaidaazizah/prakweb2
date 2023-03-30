46e8f6ab1b62c3c6e11fab681ae65a9dd9202588
ghaidaazizah <ghaidasyafaazizah@gmail.com>
2023-03-30 13:10:50 +0700

    Praktikum Week 5

--- proses_pelanggan.php
+++ proses_pelanggan.php
@@ -13,10 +13,10 @@ require_once 'dbkoneksi.php';
    $_proses = $_POST['proses'];
 
    // array data
-   $ar_data[]=$_kode; // ? 1
-   $ar_data[]=$_nama; // ? 2
-   $ar_data[]=$_jk;// 3
-   $ar_data[]=$_tmp_lahir;// 3
+   $ar_data[]=$_kode; 
+   $ar_data[]=$_nama; 
+   $ar_data[]=$_jk;
+   $ar_data[]=$_tmp_lahir;
    $ar_data[]=$_tgl_lahir;
    $ar_data[]=$_email;
    $ar_data[]=$_kartu_id;
@@ -27,7 +27,7 @@ require_once 'dbkoneksi.php';
     $sql = "INSERT INTO pelanggan (kode,nama,jk,tmp_lahir,tgl_lahir,
     email,kartu_id) VALUES (?,?,?,?,?,?,?)";
    }else if($_proses == "Update"){
-    $ar_data[]=$_POST['idedit'];// ? 8
+    $ar_data[]=$_POST['idedit'];
     $sql = "UPDATE pelanggan SET kode=?,nama=?,jk=?,tmp_lahir=?,
     tgl_lahir=?,kartu_id=? WHERE id=?";
    }
