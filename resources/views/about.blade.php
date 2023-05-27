b2cad3d14182dfa8f9324ac5c0b606d1d3740a8c
ghaidaazizah <ghaidasyafaazizah@gmail.com>
2023-05-27 23:11:50 +0700

    Praktikum Week 9

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <hr>
+    <h3>Hello, perkenalkan nama Saya {{$nama}} </h3>
+    <br>
+    <h4>Umur Saya {{$umur}} </h4>
+    <br>
+    <h4>Email Saya <?php echo $email ?> </h4>
+    <!-- bisa pake langsung {} gini biar lebih cepet, tapi kalau mau pake php juga gapapa ygy -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InputController;
+use App\Http\Controllers\ForminputController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProdukController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/salam', function () {
+    return "Selamat Siang Pak Rojul";
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "nama" => "Geda",
+        "umur" => 18,
+        "email" => "geda@gmail.com"
+    ]);
+});
+
+Route::get('/nf', function () {
+    return view('nf',[
+        "nama" => "Sekolah Tinggi Teknologi Terpadu Nurul Fikri",
+        "alamat" => "Srengseng Sawah",
+        "email" => "nurulfikri@gmail.com"
+    ]);
+});
+
+Route::get('/pemeriksaan', function () {
+    return view('pemeriksaan');
+});
+
+Route::get('/input', [InputController::class, 'index']);
+Route::post('/output', [InputController::class, 'output']);
+
+Route::get('/form', [ForminputController::class, 'index']);
+Route::post('/form', [ForminputController::class, 'form']);
+
+// Ini route untuk backend atau admin
+// cara grouping route = Route::profix('admin')->group(function)
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/produk', [ProdukController::class, 'index']);
+});
+
+// ini route untuk frontend atau user (tugas)
